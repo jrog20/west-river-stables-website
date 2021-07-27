188496a0065b1c43cbf5848ed4f2a38dbddc3599
jrog20 <jaimerogstad@gmail.com>
2021-07-27 15:31:30 -0400

    Fix render issue

--- west-river-stables-react-app/src/components/NavBar.js
+++ west-river-stables-react-app/src/components/NavBar.js
@@ -31,7 +31,7 @@ function Navbar() {
       <nav className='navbar'>
         <div className='navbar-container'>
           <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-            West River Stables 
+            West River Stables
             <i className='fab fa-typo3' />
           </Link>
           <div className='menu-icon' onClick={handleClick}>
