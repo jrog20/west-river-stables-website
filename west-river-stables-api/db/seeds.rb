2eda0bd69817823abafd88c709942089492dbf2e
jrog20 <jaimerogstad@gmail.com>
2021-07-29 16:10:40 -0400

    Add seed data

--- west-river-stables-api/db/seeds.rb
+++ west-river-stables-api/db/seeds.rb
@@ -5,4 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.create(username: 'theman', email: 'the@man.com', password: 'password')
+theman = User.create(username: 'theman', email: 'the@man.com', password: 'password')
+roger = User.create(username: 'roger', email: 'roger@roger.com', password: 'password')
+papa = User.create(username: 'papa', email: 'papa@papa.com', password: 'password')
+
+theman.secrets.create(content: 'The Man')
+roger.secrets.create(content: 'I want this website so I can upload photos')
+papa.secrets.create(content: 'I like puppies')
+papa.secrets.create(content: 'I like kitties')
