d3795b982239ea56e480796f73ba0b4037f63689
jrog20 <jaimerogstad@gmail.com>
2021-08-01 16:28:57 -0400

    Add redux middleware

--- west-river-stables-react-app/src/index.js
+++ west-river-stables-react-app/src/index.js
@@ -4,11 +4,37 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {
+  createStore,
+  applyMiddleware,
+  compose,
+  combineReducers
+} from 'redux';
+import thunk from 'redux-thunk';
+
+const currentUserReducer = (state = null, action) => {
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+     return action.user
+    default:
+      return state
+  }
+}
+
+const rootReducer = combineReducers({
+  currentUser: currentUserReducer
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(rootReducer, composeEnhancer(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   
