e507df9fdf3a7012c19e2e8e52f62f61f0471a2c
jrog20 <jaimerogstad@gmail.com>
2021-08-01 12:19:12 -0400

    Refactor create method and add helper method

--- west-river-stables-api/app/controllers/sessions_controller.rb
+++ west-river-stables-api/app/controllers/sessions_controller.rb
@@ -1,15 +1,29 @@
 class SessionsController < ApplicationController
   def create
-    # byebug
     @user = User.find_by(email: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
-      render json: @user
+      session[:user_id] = @user.id
+      response = {
+        user: @user.user_serializer
+      }
+      render json: response, status: :ok
     else
-      render json: {
-        error: "Invalid Login"
-      }, status: :unauthorized
+      response = {
+        error: "Invalid login",
+        details: @user.errors.full_messages
+      }
+      render json: response, status: :unauthorized
     end
+  end
 
+  def get_current_user
+    if logged_in?
+      render json: {
+          user: current_user.user_serializer
+        }, status: :ok
+    else
+      render json: {error: "No current user"}
+    end
   end
 
   def delete
