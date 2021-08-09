eadf6ca6a4155d73332f84e46c9243fedad189eb
jrog20 <jaimerogstad@gmail.com>
2021-08-09 10:11:54 -0400

    Fix typo in function name

--- west-river-stables-react-app/src/reducers/currentUser.js
+++ west-river-stables-react-app/src/reducers/currentUser.js
@@ -1,4 +1,4 @@
-export default function currentUserReducer(state = null, action) {
+export default function currentUser(state = null, action) {
   switch (action.type) {
     case "SET_CURRENT_USER":
      return action.user
